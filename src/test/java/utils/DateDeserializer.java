a30e6380f3c819787cad2fbe8a35440c7744ca35
Administrator <admin@example.com>
2023-09-13 18:39:44 +0300

    обновил pom

--- src/test/java/utils/DateDeserializer.java
+++ src/test/java/utils/DateDeserializer.java
@@ -1,6 +1,5 @@
 package utils;
 
-import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
@@ -12,7 +11,7 @@ import java.time.format.DateTimeFormatter;
 public class DateDeserializer extends JsonDeserializer<LocalDateTime> {
 
     @Override
-    public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JacksonException {
+    public LocalDateTime deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
         return LocalDateTime.parse(jsonParser.getText(), formatter);
     }
